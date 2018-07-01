dee489e298cf41029c41f6039b88e5075254d51b
Christophe Clerc <ch_clerc@yahoo.fr>
2018-07-01 16:23:58 +0200

    Implementing item #2: Speedtest support Starting configuration GUI

---
+++ src/cclerc/cat/view/ConfigureSpeedTestDialog.java
@@ -0,0 +1,76 @@
+package cclerc.cat.view;
+
+import cclerc.cat.Cat;
+import cclerc.cat.Configuration.Configuration;
+import cclerc.services.Constants;
+import cclerc.services.Display;
+import cclerc.services.Preferences;
+import cclerc.services.Utilities;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.util.Optional;
+
+public class ConfigureSpeedTestDialog {
+
+    private static ConfigureSpeedTestDialog configureSpeedTestDialogInstance;
+
+    // Display management
+    private static Stage dialogStage = new Stage();
+
+    /**
+     * Creates instance of ConfigureSpeedTestDialog controller
+     * @param aInParentStage Parent stage of configure speed test dialog stage
+     */
+    public static ConfigureSpeedTestDialog getInstance(Stage aInParentStage) {
+
+        FXMLLoader lDialogLoader = new FXMLLoader();
+
+        try {
+
+            // Load the fxml file and create a new stage for the popup dialog.
+            lDialogLoader.setLocation(Cat.class.getResource("view/ConfigureSpeedTestDialog.fxml"));
+            lDialogLoader.setResources(Display.getViewResourceBundle());
+            VBox lDialogPane = lDialogLoader.load();
+
+            // Create the dialog stage
+            dialogStage.initModality(Modality.WINDOW_MODAL);
+            dialogStage.initOwner(aInParentStage);
+            Scene lScene = new Scene(lDialogPane);
+            lScene.getStylesheets().add("resources/css/view.css");
+            dialogStage.setScene(lScene);
+            dialogStage.getIcons().add(Constants.APPLICATION_IMAGE);
+            dialogStage.setResizable(false);
+            dialogStage.setTitle(Display.getViewResourceBundle().getString("configureSpeedTest.title"));
+            configureSpeedTestDialogInstance = lDialogLoader.getController();
+
+        } catch (Exception e) {
+            Display.getLogger().error(String.format(Display.getMessagesResourceBundle().getString("log.cat.error.displayDialog"), Utilities.getStackTrace(e)));
+        }
+
+        return configureSpeedTestDialogInstance;
+
+    }
+
+    /**
+     * Closes dialog box
+     */
+    @FXML
+    public void close() {
+        dialogStage.close();
+    }
+
+    /**
+     * Displays the dialog box
+     */
+    public void show() {
+        dialogStage.showAndWait();
+    }
+
+}
